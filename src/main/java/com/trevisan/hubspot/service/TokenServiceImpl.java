488c6c9f6f7a79daf5ebdc330fdbadda7378b157
Diego Trevisan <diego.trevisan.cc@gmail.com>
2025-03-27 10:43:47 -0300

    feat: added requisition to oauth/v1/token endpoint

--- src/main/java/com/trevisan/hubspot/service/TokenServiceImpl.java
+++ src/main/java/com/trevisan/hubspot/service/TokenServiceImpl.java
@@ -3,12 +3,40 @@ package com.trevisan.hubspot.service;
 import com.trevisan.hubspot.restmessages.TokenRequest;
 import com.trevisan.hubspot.restmessages.TokenResponse;
 import com.trevisan.hubspot.service.interfaces.TokenService;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Service
 public class TokenServiceImpl implements TokenService {
+
+  private final WebClient webClient;
+
+  public TokenServiceImpl(WebClient.Builder webClientBuilder) {
+    this.webClient = webClientBuilder.baseUrl("https://api.hubapi.com").build();
+  }
+
   @Override
   public TokenResponse fetchToken(TokenRequest tokenRequest) {
-    return null;
+
+    String requestBody = String.format(
+        "grant_type=%s&code=%s&redirect_uri=%s&client_id=%s&client_secret=%s",
+        tokenRequest.grantType(),
+        tokenRequest.code(),
+        tokenRequest.redirectUrl(),
+        tokenRequest.clientId(),
+        tokenRequest.clientSecret()
+    );
+
+    TokenResponse response = webClient.post()
+        .uri("/oauth/v1/token")
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .bodyValue(requestBody)
+        .retrieve()
+        .bodyToMono(TokenResponse.class)
+        .block();
+
+    return response;
   }
 }
